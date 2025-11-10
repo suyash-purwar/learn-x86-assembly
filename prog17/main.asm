ca9c5ec8f583223c984e6c9fdf34ff907c37b2c7
suyash-purwar <purwarsuyash06@gmail.com>
2025-11-10 23:43:32 +0530

    practice base pointer and local procedure variables

---
+++ prog17/main.asm
@@ -0,0 +1,47 @@
+section .text
+global _start
+
+_start:
+    PUSH 0x6
+    PUSH dword "Parv"
+    CALL greet
+    ADD esp, 0x8
+
+    MOV eax, 0x1
+    MOV ebx, 0x1
+    INT 0x80
+
+greet:
+    PUSH ebp
+    MOV ebp, esp
+
+    PUSH 0x4
+    PUSH dword "Hey "
+    CALL print
+    ADD esp, 0x8
+
+    PUSH dword [ebp+12]
+    PUSH dword [ebp+8]
+    CALL print
+    ADD esp, 0x8
+
+    MOV esp, ebp
+    POP ebp
+
+    RET
+
+print:
+    PUSH ebp
+    MOV ebp, esp
+
+    MOV eax, 0x4
+    MOV ebx, 0x1
+    LEA ecx, [ebp+8]
+    MOV edx, [ebp+12]
+
+    INT 0x80
+
+    MOV esp, ebp
+    POP ebp
+
+    RET
